655f40eaed5a5a2202a3b1f7fc9d8f6ef2a37c39
David Gagnon <toogreen@gmail.com>
2020-06-25 07:53:00 +0000

    made some styling changes for the form and optimized insert code

--- inc/insert_blow.php
+++ inc/insert_blow.php
@@ -1,25 +1,8 @@
 <?php
-//include("connect_db.php");
+include("connect_db.php");
 
 
-$servername = "localhost";
-$username = "toogreen";
-$password = "toosad";
-$dbname = "blower";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-
-
-
-
-
-$sql = "INSERT INTO blow (company, comment, position) VALUES ('company', 'comment', 'position')";
+$sql = "INSERT INTO blow (company, comment, position) VALUES ('$company', '$comment', '$position')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
